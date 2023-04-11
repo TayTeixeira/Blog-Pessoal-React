e285afc4a2dc9888fa5c63186ebd51528f226204
Tay Teixeira <taymaratda@gmail.com>
2023-04-10 21:12:38 -0300

    Componentes Home e estilização CSS

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className='titulo'>Home</h1>
+            <img src="https://img.freepik.com/vetores-gratis/linguagens-de-programacao-css-e-html-programacao-de-computadores-codificacao-ti-personagem-de-desenho-animado-de-programador-feminino-software-desenvolvimento-de-sites-ilustracao-em-vetor-conceito-metafora-isolado_335657-2740.jpg?w=740&t=st=1681171036~exp=1681171636~hmac=70998ce3f7ee63cbfdfdfe80d6ca13ed2308bb0e980796035e8fb26879c14167" alt="Imagem Tela Inicial" className='img' />
+        </>
+    );
+}
+
+export default Home;
