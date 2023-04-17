2e6e10b6c5f1259233eae55a5ed5c389379155c3
Tay Teixeira <taymaratda@gmail.com>
2023-04-17 15:22:51 -0300

    Implementação de segurança

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Footer from './componentes/estaticos/footer/Footer';
 import { Grid } from '@material-ui/core';
 import Home from './paginas/home/Home';
 import './App.css';
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 import Login from './paginas/login/Login';
 
 function App() {
@@ -16,7 +17,7 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Home />} />
-          {/* <Route path="/cadastro" element={<CadastroUsuario />} /> */}
+          <Route path="/cadastrousuario" element={<CadastroUsuario />} />
         </Routes>
       </div>
       <Footer />
--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -1,32 +1,73 @@
-import React from "react";
-import {Grid, Typography, TextField} from '@material-ui/core';
+import React, { useState, useEffect, ChangeEvent } from "react";
+import { Grid, Typography, TextField } from '@material-ui/core';
 import { Box, Button } from "@mui/material";
 import './Login.css'
 import { TextFields } from "@mui/icons-material";
-import { Link } from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import UserLogin from "../../models/UserLogin";
+import useLocalStorage from "react-use-localstorage";
+import { login } from "../../services/Service";
 
-function Login () {
+function Login() {
+    let history = useNavigate();
+    const [token, setToken] = useLocalStorage('token');
+    const [userLogin, setUserLogin] = useState<UserLogin>(
+        {
+            id: 0,
+            nome: '',
+            usuario: '',
+            senha: '',
+            foto: '',
+            token: ''
+        }
+    )
+
+    function updatedModel(e: ChangeEvent<HTMLInputElement>) {
+
+        setUserLogin({
+            ...userLogin,
+            [e.target.name]: e.target.value
+        })
+    }
+
+            useEffect(() =>{
+                if (token != ''){
+                    history('/home')
+                }
+            }, [token])
+
+    async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
+        e.preventDefault();
+        try {
+            await login('/usuarios/logar', userLogin, setToken)
+
+            alert('Usuário logado com sucesso!');
+        } catch (error) {
+            alert('Dados do usuário inconsistentes. Erro ao logar!')
+        }
+    }
+
     return (
         <Grid container direction="row" justifyContent="center" alignItems="center">
             <Grid item xs={6}>
                 <Box paddingX={20}>
-                    <form>
+                    <form onSubmit={onSubmit}>
                         <Typography variant="h3" gutterBottom color="textPrimary" align="center" className="textos-login">Entrar</Typography>
-                        <TextField id='usuario' label='usuário' variant='outlined' name='usuario' margin='normal' fullWidth/>
-                        <TextField id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth/>   
+                        <TextField value={userLogin.usuario} onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' label='usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
+                        <TextField value={userLogin.senha} onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
                         <Box marginTop={2} textAlign='center'>
-                            <Link to='/home' className="text-decorator-none">
-                                <Button type='submit' variant='contained' className='botao-login'>
-                                    Logar
-                                </Button>
-                            </Link>
+                            <Button type='submit' variant='contained' className='botao-login'>
+                                Logar
+                            </Button>
                         </Box>
                     </form>
                     <Box display='flex' justifyContent='center' marginTop={2}>
                         <Box marginRight={1}>
                             <Typography variant="subtitle1" gutterBottom align="center">Não tem uma conta?</Typography>
                         </Box>
-                        <Typography variant="subtitle1" gutterBottom align="center" className="textos-login">Cadastre-se</Typography>
+                        <Link to='/cadastrousuario'>
+                            <Typography variant="subtitle1" gutterBottom align="center" className="textos-login">Cadastre-se</Typography>
+                        </Link>
                     </Box>
                 </Box>
             </Grid>
